05a4be4287abc764fd6410c4c72869188eafc2e7
Tong Li <tli0312@gmail.com>
2013-07-08 07:18:06 +0000

    Update web.js to read from index.html file

--- web.js
+++ web.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var fs = require('fs');
+
+var fileName = "index.html"
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World From Bitstarter!');
+
+ fs.readFile(fileName, function(error, buffer) {
+   if (!error) {
+     var data = buffer.toString("utf8", 0, buffer.length);
+     console.log(data);
+     response.send(data);
+   }
+   else {
+     console.error("No File Found");
+     response.send("No File Found");
+     }
+     }); 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8888;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
